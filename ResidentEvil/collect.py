5024120e8869bba723b3edbfcc1f270d0af0c9de
Gabriel Alexandre <gabriel.zerraa@gmail.com>
2024-06-24 11:04:57 -0300

    Coleta de Dados dos personagens do RE

--- ResidentEvil/collect.py
+++ ResidentEvil/collect.py
@@ -2,15 +2,13 @@
 
 import requests
 from bs4 import BeautifulSoup
-
-url = 'https://www.residentevildatabase.com/personagens/albert-wesker/'
+from tqdm import tqdm
+import pandas as pd
 
 headers = {
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
     'accept-language': 'pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7',
     'cache-control': 'max-age=0',
-    # Requests sorts cookies= alphabetically
-    # 'cookie': '_gid=GA1.2.510509082.1718997808; _ga=GA1.1.1367306953.1718997807; __gads=ID=06b102cd787d8d6f:T=1718997851:RT=1719000161:S=ALNI_MYpaH55AmS_-VwYgRYWq1HGrMtmHA; __gpi=UID=00000a2fee99d4a4:T=1718997851:RT=1719000161:S=ALNI_MbJzx03MfKqff5K7Dcuj2ZiUFTbSQ; __eoi=ID=fa734989e0a93171:T=1718997851:RT=1719000161:S=AA-AfjaJY0FwwvMTdc8vyMmoOlUk; _ga_DJLCSW50SC=GS1.1.1719000117.2.1.1719000121.56.0.0; _ga_D6NF5QC4QT=GS1.1.1719000117.2.1.1719000121.56.0.0; FCNEC=%5B%5B%22AKsRol8n8zRzZZvFCupdTA-_lWEdIvqSZmb0Kb6fl54836qxlFqZPFzylIAXHUyKqGLdqplBabFzurl83Tl4wXfrdjuOiFz45LcRgzJtWsEtYqxM6cMiJT_kVBV5RVedGDKbmgxh5xSUQgIeRGGT6cGTEapTr5PXtA%3D%3D%22%5D%5D',
     'priority': 'u=0, i',
     'referer': 'https://www.residentevildatabase.com/personagens/',
     'sec-ch-ua': '"Not/A)Brand";v="8", "Chromium";v="126", "Google Chrome";v="126"',
@@ -24,29 +22,74 @@ headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
 }
 
-resp = requests.get(url, headers=headers)
+def get_content(url):
+    resp = requests.get(url, headers=headers)
+    return resp
+
+def get_basic_infos(soup):
+    div_page = soup.find('div', class_ = 'td-page-content')
+    paragrafo = div_page.find_all('p')[1]
+    ems = paragrafo.find_all('em')
+    data = {}
+    for i in ems:
+        chave, valor, *_ = i.text.split(':')
+        chave = chave.strip(' ')
+        data[chave] = valor.strip(' ')
+    return data
+
+def get_aparicoes(soup):
+    lis = (soup.find('div', class_ = 'td-page-content')
+            .find('h4')
+            .findNext()
+            .find_all('li'))
+
+    aparicoes = [i.text for i in lis]
+    return aparicoes
+
+def get_personagens_infos(url):
+    resp = get_content(url)
+    if resp.status_code != 200:
+        print('Não foi possível obter os dados')
+        return {}
+    else:
+        soup = BeautifulSoup(resp.text)
+        data = get_basic_infos(soup)
+        data['Aparicoes'] = get_aparicoes(soup)
+        return data
+    
+def get_links():
+    url = 'https://www.residentevildatabase.com/personagens/'
+    resp = requests.get(url, headers=headers)
+    soup_personagem = BeautifulSoup(resp.text)
+    ancoras = (soup_personagem.find('div', class_='td-page-content')
+            .find_all('a'))
+
+    links = [i['href'] for i in ancoras]
+    return links
 
 # %%
+links = get_links()
+data = []
+for i in tqdm(links):
+    print(i)
+    d = get_personagens_infos(i)
+    d['link'] = i
+    nome = i.strip("/").split("/")[-1].replace("-", " ").title()
+    d['nome'] = nome
+    data.append(d)
 
-soup = BeautifulSoup(resp.text)
-soup
+
 # %%
-div_page = soup.find('div', class_ = 'td-page-content')
-# %%
-paragrafo = div_page.find_all('p')[1]
-paragrafo
-# %%
-ems = paragrafo.find_all('em')
-ems
-# %%
-ems[0].text
-# %%
-data = {}
-for i in ems:
-    chave, valor = i.text.split(':')
-    chave = chave.strip(' ')
-    data[chave] = valor.strip(' ')
 data
 # %%
-data['Peso']
+df = pd.DataFrame(data)
+df
 # %%
+df[~df['de nascimento'].isna()]
+# %%
+df.to_csv('Resident_Evil_Personagens.csv', index=False)
+# %%
+df.to_parquet('Resident_Evil_Personagens.parquet', index=False)
+# %%
+df.to_pickle('Resident_Evil_Personagens.pkl')
+# %
